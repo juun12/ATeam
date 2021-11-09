ff6d150478c9b27ba4751a6006989c5b80beabad
001-14\hanul <jklee1224@naver.com>
2021-11-09 16:07:28 +0900

    20211108

--- studyjsp/webcontent/src/MemberJoin.java
+++ studyjsp/webcontent/src/MemberJoin.java
@@ -1,11 +1,16 @@
 
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.hanul.member.MemberDTO;
+import com.sun.xml.internal.bind.v2.runtime.Name;
 
 /**
  * Servlet implementation class MemberJoin
@@ -21,26 +26,29 @@ public class MemberJoin extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-//테스트용
+
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		//① 클라이언트의 요청을 받는다 : 매개변수를 가져온다 ▶ HttpServletRequest
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 클라이언트 요청을 받는다: 매개변수를 가져온다😍🤢 ->HttpServletRequest
 		request.setCharacterEncoding("utf-8");
-		String name = request.getParameter("name");
-		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
-		int age = Integer.parseInt(request.getParameter("age"));
-		String addr = request.getParameter("addr");
-		
-		System.out.println("이름 : " + name);
-		System.out.println("아이디 : " + id);
-		System.out.println("비밀번호 : " + pw);
-		System.out.println("나이 : " + age);
-		System.out.println("주소 : " + addr);
+String name =request.getParameter("name");
+String id =request.getParameter("id");
+String pw =request.getParameter("pw");
+	int age =Integer.parseInt(request.getParameter("age"));
+String addr = request.getParameter("addr");
+System.out.println("이름:"+name);
+System.out.println("아이디:"+id);
+System.out.println("비밀번호:"+pw);
+System.out.println("나이:"+age);
+System.out.println("주소:"+addr);
+	MemberDTO dto= new MemberDTO(name,id,pw,age,addr);
+	request.setAttribute("dto", dto);
+	RequestDispatcher rd = request.getRequestDispatcher("result.jsp");
+	rd.forward(request, response);
 	}
+	
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
